81399c83cfac9052ae20b94c789c9cccbd7d8873
Erick Maldonado <erickbm303@gmail.com>
2026-05-26 02:06:02 -0400

    Finalized STA 3000, ai-related stock analysis, more research needs to be done

--- scripts/01_dowload_and_check_data.R
+++ scripts/01_dowload_and_check_data.R
@@ -1,9 +1,8 @@
-# Install packages only once
-install.packages("tidyquant")
-install.packages("dplyr")
+
 
 library(tidyquant)
 library(dplyr)
+library(ggplot2)
 
 # 1. Define tickers
 tickers <- c("AAPL", "AMZN", "GOOGL", "TSLA", "META", "MSFT", "NVDA", "SPY")
@@ -11,16 +10,12 @@ tickers <- c("AAPL", "AMZN", "GOOGL", "TSLA", "META", "MSFT", "NVDA", "SPY")
 # 2. Download daily stock price data from Yahoo Finance
 prices <- tq_get(
   tickers,
-  from = "2016-01-01",
+  from = "2020-01-01",
   to = Sys.Date(),
   get = "stock.prices"
 )
-
 # 3. Check the data
 head(prices)
-
-install.packages("ggplot2")
-library(ggplot2)
 
 ggplot(prices, aes(x = date, y = adjusted, color = symbol)) +
   geom_line() +
